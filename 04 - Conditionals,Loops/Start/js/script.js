f77fab007143385f46493705fbd9eca673568b88
Dmitriy Onischenko <donischenko801@gmail.com>
2017-06-14 11:44:40 +0300

    Add js homework

---
+++ 04 - Conditionals,Loops/Start/js/script.js
@@ -0,0 +1,89 @@
+console.clear();
+
+// ## 0 Проверка диапазона
+//
+// Напишите код который б проверял длинну телефонного номера
+// Длинна номера не должна быть меньше 9 и больше 11
+
+// * Your code Start *
+
+
+
+// * Your code End *
+
+
+// ## 1 Клонирование обьектов
+//
+// Склонируйте обьект objectA в objectB при помощи цикла
+
+var objectA = {
+  name: 'John',
+  lastname: 'Dou',
+  age: 50
+}
+
+var objectB = {};
+
+// * Your code Start *
+
+
+
+// * Your code End *
+
+// Переопределяем значения обьекта objectA
+objectA.name = 'Chris';
+objectA.name = 'Couer';
+objectA.name = '45';
+
+// Если обьект objectB склонирован, значения в нем меняться не будут
+// Выводим значения склонированного обьекта для проверки
+console.log(objectB.name); // John
+console.log(objectB.lastname); // Dou
+console.log(objectB.age); // 50
+
+
+// ## 2 Ветвление
+// При помощи конструкци switch/case выведете текущую дату
+// Название месяца должно иметь вид строки
+// Например = 14 Июня 2017
+
+var date = new Date();
+var day = date.getDate(); // Текущий день
+var month = date.getMonth(); // Текущий месяц (от 0 до 11)
+var year = date.getFullYear(); // Текущий год
+var monthName = ''; // Переменная для значения
+
+// * Your code Start *
+
+
+
+// * Your code End *
+
+console.log(`Today is: ${day} ${monthName} ${year}`) // Пример: 14 Июня 2017
+
+
+
+// ## 3 Взаимодействие с пользователем
+//
+// Запросите через prompt у пользователя его имя;
+// Запрашивайте имя пользователя, пока одно из имен не будет совпадать с одним из массива users
+// После того как пользователь введет имя которое есть в обьекте users, запросите у него пароль
+// -- Если пароль введен не верно выведете alert об этом и вернитесь на повторый ввод имени
+// -- Если пароль совпадает - вывести сообщение о том что пароль верный и прервите цикл
+
+var users = [{
+  name: 'Alex',
+  password: 123
+}, {
+  name: 'Chris',
+  password: 456
+}, {
+  name: 'Bill',
+  password: 789
+}];
+
+// * Your code Start *
+
+
+
+// * Your code End *
