0952f9d0a6ec999393feb3e31dc5d5adc117fb73
Dmitriy Onischenko <donischenko801@gmail.com>
2017-07-01 22:53:54 +0300

    Add homework task

--- 05 - Function:Arrays_ethods/Start/js/script.js
+++ 05 - Function:Arrays_ethods/Start/js/script.js
@@ -11,13 +11,13 @@ console.clear();
 
 // * Your code End *
 
-console.log(argumentsSumm(10, 20, 50, 4, 17))   // 97
-console.log(argumentsSumm(0, 1, 1, -1, 1))      // 3
-console.log(argumentsSumm(4, 'lol', 0))         // Все аргументы должны быть числовыми
-console.log(argumentsSumm(NaN, 4, 6, 4, 0))     // Все аргументы должны быть числовыми
+//console.log(argumentsSumm(10, 20, 50, 4, 17))   // 97
+//console.log(argumentsSumm(0, 1, 1, -1, 1))      // 3
+//console.log(argumentsSumm(4, 'lol', 0))         // Все аргументы должны быть числовыми
+//console.log(argumentsSumm(NaN, 4, 6, 4, 0))     // Все аргументы должны быть числовыми
 
 
-// ## 0 Замыкания
+// ## 1 Замыкания
 //
 // Посмотрите на код внизу
 // Обьясните почему он работает именно так
@@ -44,3 +44,18 @@ var func = funcArrays();
 
 console.log(func[1]()); // Выводит 5, а должно вывести 1
 console.log(func[3]()); // Выводит 5, а должно вывести 3
+
+
+// ## Работа с массивами
+// Совсестите массив и строку с именами именно в таком порядке, как показанно в комментариях в строке%.
+
+var oldArray = ['Dave Burt', 'Sylvanus Jeremiah'];
+var namesString = 'Hartley Evander, Kole Grady';
+
+// * Your code Start *
+
+
+
+// * Your code End *
+
+console.log(oldArray.reverse()) // Должен вывести - 'Dave Burt | Hartley Evander | Kole Grady | Sylvanus Jeremiah'
