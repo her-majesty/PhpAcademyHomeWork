fc08b0db979cc947126f1f9441952493effc40d6
Dmitriy Onischenko <donischenko801@gmail.com>
2017-07-23 11:51:12 +0300

    add last home task

--- 07 - Context/Start/js/script.js
+++ 07 - Context/Start/js/script.js
@@ -35,3 +35,39 @@ console.log(personProp()); // Alex Murphy - неверно
 // * Your Code End *
 
 console.log(personProp()); // Должно вывести 'William Mason'
+
+
+// ## 1 Метод apply
+//
+// Найдите минимальное и максимальное число посредством метода apply
+// Так же не забывайте что существует обьект Math
+
+var  numbers = [20, 386 , 512 , 17 , -228 , 590 , 323647, -3424256, 55657];
+
+// * Your Code Start *
+
+
+
+// * Your Code End *
+
+console.log(maxInNumbers) // 323647
+console.log(minInNumbers) // -3424256
+
+// ## 2 метод call
+//
+// Создайте функцию которая сортировала и возврашала аргументы отсортированные по алфавиту (от A до Z)
+// и по числам (по возрастанию) если все агументы числовые
+// Используйте для этого метод call
+// Обратите внимание на регист в строках
+
+// * Your Code Start *
+
+
+
+// * Your Code End *
+
+console.log(sortedArgument(12, 4, 6, 8, 56, 7, 4, 7)) // [4, 4, 6, 7, 7, 8, 12, 56]
+console.log(sortedArgument('Lorem', 'ipsum', 'dolor', 'Sit', 'amet', 'consectetur', 'adipisicing', 'elit')) // ["adipisicing", "amet", "consectetur", "dolor", "elit", "ipsum", "lorem", "sit"]
+
+// ## 3 Карринг
+//
