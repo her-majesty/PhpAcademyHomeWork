121bbbaccd3723c7ea606f9303225d51b7001e45
Dmitriy Onishchenko <donischenko801@gmail.com>
2017-09-04 01:26:31 +0300

    finish hometask

--- 09 - Regex,Console,Testing/Start/js/script.js
+++ 09 - Regex,Console,Testing/Start/js/script.js
@@ -1,6 +1,6 @@
 console.clear();
 
-// Работа c методами обьекта console
+// *** Работа c методами обьекта console ***
 // 
 // * Попробуйте для каждого вида данных использовать свой 
 // метод обьекта console
@@ -11,7 +11,7 @@ const styledMessage = 'This message must be red, and font size must be 22px'
 const infoMessage = 'This is an info message.';
 const warningMessage = 'This is an warning message.';
 const errorMessage = 'This is an error message.';
-const assert = 1 === '1';
+const asserts = 1 === '1';
 const objects = [{
         firstName: 'Alex',
         lastName: 'Simpson',
@@ -50,8 +50,8 @@ const objects = [{
 // * Your Code End *
 
 
-// * Выведите все елементы обьекта поочередно в цикеле, 
-// обьеденив в группы по имени
+// * Выведите все елементы обьекта поочередно в цикле, 
+// обьеденив в группы, по имени
 // ============================================================
 objects.forEach(function(item) {
   // * Your Code Start *
@@ -61,3 +61,58 @@ objects.forEach(function(item) {
   // * Your Code End *
 });
 
+
+// *** Регулярные выражения ***
+//
+// * Найдите евсе совпадения 'dolor' в подстроке
+var dolors = `Lorem ipsum dolor sit amet, consectetur 
+adipisicing elit. Sed odio quisquam doloremque 
+dignissimos vitae accusantium neque eveniet 
+necessitatibus nulla quas est sint libero, 
+aperiam autem labore delectus! Sed, ipsum, cupiditate.`;
+
+// * Your Code Start *
+
+
+
+  // * Your Code End *
+
+
+// Удаление пробелов
+// ============================================================
+// * Напишите regex который удалял бы все проблеы в начале и в конце строк
+// * Так же все пробелы внутри строки которых более одного заменял бы на 1 пробел
+
+var trimString = '   Lorem      ipsum dolor  sit amet,        consectetur   adipisicing elit     '; // => "Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+
+// * Your Code Start *
+
+
+
+  // * Your Code End *
+
+// Замена в цикле
+// ============================================================
+// * Создайте цикл который используя регулярные выражения проходил 
+// * по всем "путям" и заменял их на такие как указаны в комментариях
+var parts = [
+  './img/image1.jpg',    // => ./img/image1.jpg
+  'https://images.google.com/images/../img/image2.png',    // => ./img/image2.png
+  '.\img/image3.gif',    // => ./img/image3.gif
+  'localhos/img/image4.png',    // => ./img/image4.png
+  '../image5.psd',    // => ./img/image5.psd
+];
+
+// * Your Code Start *
+
+
+
+// * Your Code End *
+
+
+// *** Unit тестирование (TDD) ***
+//
+// Напришите 3 теста для проверки полей абстрактной формы логина (Fields: Login\Password\Email)
+// Реализовывать методы валидации и тд не нужно.
+// Представьте что вы используете подход TDD. (Сначала тесты, потом реализация)
+// Тесты должны быть описанны в файле test.js
---
+++ 09 - Regex,Console,Testing/Start/js/test.js
@@ -0,0 +1,25 @@
+
+// Tests for login
+// ============================================================
+// * Your Code Start *
+
+
+
+// * Your Code End *
+
+
+// Tests for password
+// ============================================================
+// * Your Code Start *
+
+
+
+// * Your Code End *
+
+// Tests for email
+// ============================================================
+// * Your Code Start *
+
+
+
+// * Your Code End *
