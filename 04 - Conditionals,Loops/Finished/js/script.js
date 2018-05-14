c31268d964ea709f457279b2df85a340c0cafc27
julia <kuznietsova.j@gmail.com>
2018-05-14 22:41:03 +0300

    Finished 05 homework

--- 04 - Conditionals,Loops/Finished/js/script.js
+++ 04 - Conditionals,Loops/Finished/js/script.js
@@ -5,7 +5,7 @@ console.clear();
 // Напишите код который б проверял длинну телефонного номера
 // Длинна номера не должна быть не меньше 9 и не больше 11
 
-function checkPhoneLength (phone) {
+function checkPhoneLength(phone) {
   if (phone.length >= 9 && phone.length <= 11) return "Phone number is correct";
   else return "Please enter corect phone number";
 }
@@ -54,30 +54,42 @@ var year = date.getFullYear(); // Текущий год
 var monthName = ''; // Переменная для значения
 
 switch (month) {
-  case 0: monthName = "of January";
-  break;
-  case 1: monthName = "of February"
-  break;
-  case 2: monthName = "of March";
-  break;
-  case 3: monthName = "of April";
-  break;
-  case 4: monthName = "of May";
-  break;
-  case 5: monthName = "of June";
-  break;
-  case 6: monthName = "of July";
-  break;
-  case 7: monthName = "of August";
-  break; 
-  case 8: monthName = "of September";
-  break;
-  case 9: monthName = "of October";
-  break;
-  case 10: monthName = "of November";
-  break;
-  case 11: monthName = "of December";
-  break;
+  case 0:
+    monthName = "of January";
+    break;
+  case 1:
+    monthName = "of February"
+    break;
+  case 2:
+    monthName = "of March";
+    break;
+  case 3:
+    monthName = "of April";
+    break;
+  case 4:
+    monthName = "of May";
+    break;
+  case 5:
+    monthName = "of June";
+    break;
+  case 6:
+    monthName = "of July";
+    break;
+  case 7:
+    monthName = "of August";
+    break;
+  case 8:
+    monthName = "of September";
+    break;
+  case 9:
+    monthName = "of October";
+    break;
+  case 10:
+    monthName = "of November";
+    break;
+  case 11:
+    monthName = "of December";
+    break;
 }
 
 console.log(`Today is: ${day} ${monthName} ${year}`) // Пример: 14 Июня 2017
@@ -105,34 +117,32 @@ var users = [{
 
 // Solution # 1
 
-function checkSignInData () {
-  
+function checkSignInData() {
+
   let userName;
   let password;
-  
+
   userName = prompt('Please enter your user name', '');
-  
+
   if (userName == '' || userName == null) return "Authorization was stopped";
-  
+
   for (var i = 0; i <= users.length; i++) {
-    
-      if (i == users.length) return checkSignInData ();
-    
-      if (userName == users[i].name) {
-        
-         passWord =  prompt('Please enter your password', '');
-        
-         if (passWord == users[i].password.toString()) {
-           
-            alert(`Access allowed! Welcome, ${users[i].name}!`);
-            return "Successful authorization!";
-         }
-          else {
-            alert('Wrong password! Please try again!');
-            return checkSignInData ();
-          };
-        }
-      else continue;
+
+    if (i == users.length) return checkSignInData();
+
+    if (userName == users[i].name) {
+
+      passWord = prompt('Please enter your password', '');
+
+      if (passWord == users[i].password.toString()) {
+
+        alert(`Access allowed! Welcome, ${users[i].name}!`);
+        return "Successful authorization!";
+      } else {
+        alert('Wrong password! Please try again!');
+        return checkSignInData();
+      };
+    } else continue;
   };
 };
 
@@ -140,45 +150,44 @@ console.log(checkSignInData());
 
 // Solution # 2
 
-function checkSignInData () {
-  
-   let userName;
-   let password;
-   let userIndex;
- 
-   function checkUserName () {
-  
-      while(true) {
-        
-          userName = prompt('Please enter your user name', '');
-          if (userName === '' || userName === null) break;
-         
-          for (let i = 0; i < users.length; i++) {
-          if (userName == users[i].name) return i;
-          else continue;
-          };     
-     };
-     
-return "Authorization was stopped";
-     
-};
-  
-   userIndex = checkUserName ();
-   if (userIndex === "Authorization was stopped") return "Authorization was stopped";
-  
-   password = prompt('Please enter your password', '');
-  
+function checkSignInData() {
+
+  let userName;
+  let password;
+  let userIndex;
+
+  function checkUserName() {
+
+    while (true) {
+
+      userName = prompt('Please enter your user name', '');
+      if (userName === '' || userName === null) break;
+
+      for (let i = 0; i < users.length; i++) {
+        if (userName == users[i].name) return i;
+        else continue;
+      };
+    };
+
+    return "Authorization was stopped";
+
+  };
+
+  userIndex = checkUserName();
+  if (userIndex === "Authorization was stopped") return "Authorization was stopped";
+
+  password = prompt('Please enter your password', '');
+
   if (password === users[userIndex].password.toString()) {
     alert(`Access allowed! Welcome, ${users[userIndex].name}!`);
-  }
-  else {
+  } else {
     alert('Wrong password! Please try again!');
-    checkSignInData ();
+    checkSignInData();
   };
   return "Successful authorization!";
 };
 
-console.log (checkSignInData ());
+console.log(checkSignInData());
 
 
 // // ## 4
@@ -186,32 +195,32 @@ console.log (checkSignInData ());
 // // Цикл прирывается только в том случае если пользователь ввел что либо кроме числа
 // // После прерывания цикла выводится сумма введеных чисел
 
-function sumEnteredNumbers () {
+function sumEnteredNumbers() {
 
-var sumArray;
-let sum = 0;
-  
-while(true) {
-  
-  sumArray = prompt('Please enter number', 0);
-  
-  if (isNaN(sumArray) || sumArray === null) break;
-  sum+=(+sumArray);
-}
+  var sumArray;
+  let sum = 0;
+
+  while (true) {
+
+    sumArray = prompt('Please enter number', 0);
+
+    if (isNaN(sumArray) || sumArray === null) break;
+    sum += (+sumArray);
+  }
   return sum;
 };
 
 console.log(sumEnteredNumbers());
-  
+
 // // ## 5
 // // Найдите сумму  1 + 2 + 3 + ... + N, где число N вводится пользователем из prompt
 
 var summetNumber = +prompt('Please enter number', 0);
 
-function sum () {
+function sum() {
   let sum = 0;
   for (let i = 1; i <= summetNumber; i++) {
-    sum+=i;
+    sum += i;
   }
   return sum;
 }
@@ -227,39 +236,37 @@ console.log(sum(summetNumber));
 // // Например что бы нарисовать 1 квадрат нужно вызвать ее 1 раз: drawFraction();
 // // Так же доски должны иметь черный\белый цвет. Реализовать это можно при помощи как css так и js
 
-var drawFraction = function(){
+var drawFraction = function () {
   const board = document.querySelector('.board');
   const fraction = document.createElement('div');
   board.appendChild(fraction);
-  }
+}
 
 const size = 8;
 
 
-for (let i = 1, k = i -1; i <= size; i++) {
+for (let i = 1, k = i - 1; i <= size; i++) {
 
-   if (i % 2 == 0) {
-      
-        for (let j = 1; j <= size; j++, k++) {
-            drawFraction();
-            if (j % 2 == 0) {
-              document.querySelectorAll('.board > div')[k].classList.add('black');
-            }
-            else {
-              document.querySelectorAll('.board > div')[k].classList.remove('black');
-            };
-        };
-     
-   } else {
-     
-          for (let j = 1; j <= size; j++, k++) {
-             drawFraction();
-             if (j % 2 == 0) {
-               document.querySelectorAll('.board > div')[k].classList.remove('black');
-             }
-             else {
-               document.querySelectorAll('.board > div')[k].classList.add('black');
-             };
-         };
+  if (i % 2 == 0) {
+
+    for (let j = 1; j <= size; j++, k++) {
+      drawFraction();
+      if (j % 2 == 0) {
+        document.querySelectorAll('.board > div')[k].classList.add('black');
+      } else {
+        document.querySelectorAll('.board > div')[k].classList.remove('black');
+      };
     };
+
+  } else {
+
+    for (let j = 1; j <= size; j++, k++) {
+      drawFraction();
+      if (j % 2 == 0) {
+        document.querySelectorAll('.board > div')[k].classList.remove('black');
+      } else {
+        document.querySelectorAll('.board > div')[k].classList.add('black');
+      };
+    };
+  };
 };
